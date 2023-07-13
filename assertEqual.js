32a1809c3adce2c3ca46654e57041823097ac13d
Jake Lollback <jalollback@gmail.com>
2023-07-12 22:03:49 -0500

    Improvement to concatenation in this function by using template literals

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,9 @@
 const assertEqual = function(actual, expected) {
 
   if (actual === expected) {
-    console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else if (actual !== expected) {
-    console.log("👎 👎 👎  Assertion Failed: " + actual + " !== " + expected);
+    console.log(`👎 👎 👎 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 

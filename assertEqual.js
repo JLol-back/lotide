96c135c222f4c89c2cc2ae32166bbc1eb9b52d51
Jake Lollback <jalollback@gmail.com>
2023-07-12 21:24:24 -0500

    Implementation of assertEqual - M1W1

---
+++ assertEqual.js
@@ -0,0 +1,18 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
+  } else if (actual !== expected) {
+    console.log("👎 👎 👎  Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+// TEST CODE
+
+assertEqual("Jake", "Lollback");
+assertEqual(1, 1);
+assertEqual("Jake", "Jake");
+assertEqual("Jake", "jake");
+assertEqual(25, 25);
+assertEqual(25, "25");
+assertEqual(25, 52);

d8035175d5522b6914838e17db7d2719bc773666
Jake Lollback <jalollback@gmail.com>
2023-07-15 09:27:16 -0500

    Implement eqArrays - Lotide - M1W1

---
+++ eqArrays.js
@@ -0,0 +1,38 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`👎 👎 👎 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+//Refactor opp: Isolate the array length checking (below) as its own function
+
+function eqArrays(array1, array2) {
+  let isEqual;
+  if (array1.length !== array2.length) {
+    isEqual = false;
+    return isEqual;
+  } else if (array1.length === array2.length) {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        //console.log(`Arrays are not equal: Array 1, position ${i} (${array1[i]}) does not equal Array 2, position ${i} (${array1[i]})`);
+        isEqual = false;
+        //console.log(isEqual);
+      } else {
+        //console.log("Arrays are equal")
+        isEqual = true;
+      }
+
+    }
+  } return isEqual;
+}
+
+// TEST CODE
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [1, 2]), true); // => should FAIL - Not the same number of elements in each array
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); // => should FAIL
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); // should FAIL
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // should PASS

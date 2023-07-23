9c2d02dec5bc12315f6a7a48d1ab6abe30854d26
Jake Lollback <jalollback@gmail.com>
2023-07-23 11:03:39 -0500

    Initial commit - Implement Tail Lotide Challenge - M1W1

---
+++ tail.js
@@ -0,0 +1,31 @@
+// Implement Tail - Lotide Challenge M1W1
+
+const tail = function(array) {
+  let newArray = array.slice(1);
+  return newArray;
+};
+
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`👎 👎 👎 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+//Test cases
+
+//Check the returned array elements
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+
+//Check original array is unmodified
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
+

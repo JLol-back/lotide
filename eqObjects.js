9e88cc7f17be4efde57fc8445e81203895d8c23a
Jake Lollback <jalollback@gmail.com>
2023-07-23 13:34:45 -0500

    Assistance request identified that I had an error in my test cases, not the eqObjects function. Corrected and tests allappear to work now.

--- eqObjects.js
+++ eqObjects.js
@@ -104,18 +104,22 @@ assertEqual(eqObjects(shirtObject , anotherShirtObject), true);
 
 const longSleeveShirtObject= { size: "medium", color: "red", sleeveLength: "long" };
 eqObjects(shirtObject , longSleeveShirtObject); // false
-assertEqual(eqObjects(shirtObject , longSleeveShirtObject), false);
+assertEqual(eqObjects(shirtObject, longSleeveShirtObject), false);
 
 
 const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
 const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] };
-assertEqual(eqObjects(multiColorShirtObject  , anotherMultiColorShirtObject)); // => true
+assertEqual(eqObjects(multiColorShirtObject, anotherMultiColorShirtObject), true); // => true
 
 const longSleeveMultiColorShirtObject= { size: "medium", colors: ["red", "blue"], sleeveLength: "long" };
-assertEqual(eqObjects(multiColorShirtObject  , longSleeveMultiColorShirtObject)); // => false
+assertEqual(eqObjects(multiColorShirtObject, longSleeveMultiColorShirtObject), false); // => false
 
 
 
+console.log("--------TEST EQARRAYS--------")
+console.log(eqArrays([1, 2, 3], [1, 2, 3]));
+console.log(eqArrays([1, 2, 3], ["1", 2, 3]));
+console.log(eqArrays([1, 2, 3], [1, 2]));
 
 
 

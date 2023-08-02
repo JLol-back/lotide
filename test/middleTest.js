9c13797ce9a0ed72ab0473f98689cd2e944f8516
Jake Lollback <jalollback@gmail.com>
2023-08-01 23:10:02 -0500

    Added test case to ensure an empty array is returned when tail.js is provided with an array with two or less elements

--- test/middleTest.js
+++ test/middleTest.js
@@ -4,16 +4,20 @@ const assert = require('chai').assert;
 
 describe("#middle", () => {
   it("returns 'Error: Not an array' if provided anything other than an array", () => {
-    assert.strictEqual(middle("string"), "Error: Not an array" );
+    assert.strictEqual(middle("string"), "Error: Not an array");
   });
+
+  it("returns an empty array when provided with an array with two or less elements", () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+  
 
   it("returns an array with a single element when provided with an array with an ODD number of elements", () => {
-    assert.deepEqual(middle([1, 2, 3]), [2] );
+    assert.deepEqual(middle([1, 2, 3]), [2]);
   });
 
-  it("returns an array with TWO elements when provided with an array with an EVEN number of elements", () => {
-    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3] );
+  it("returns an array with the TWO middle elements when provided with an array with an EVEN number of elements", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
   });
-
 
 });  

7a29ca45c5b4d181ccc4fe46019ecb4d88e574d7
Jake Lollback <jalollback@gmail.com>
2023-08-03 00:15:27 -0500

    Added index.js file to export all Lotide functions

---
+++ index.js
@@ -0,0 +1,36 @@
+const head = require('./head');
+const tail = require('./tail');
+const middle = require('./middle');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const letterPositions = require('./letterPositions');
+const findKeyByValue = require('./findKeyByValue');
+const without = require('./without');
+const flatten = require('./flatten');
+const map = require('./map');
+const takeUntil = require('./takeUntil');
+const findKey = require('./findKey');
+const eqObjects = require('./eqObjects');
+const eqArrays = require('./eqArrays');
+
+module.exports = {
+  head,
+  tail,
+  middle,
+  countOnly,
+  countLetters,
+  letterPositions,
+  findKeyByValue,
+  without,
+  flatten,
+  map,
+  takeUntil,
+  findKey,
+  eqObjects,
+  eqArrays
+};
+
+
+//assertEqual
+//assertArraysEqual
+//assertObjectsEqual

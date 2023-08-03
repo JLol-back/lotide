bcc851f737efe84e33284b549d343e898cd7d664
Jake Lollback <jalollback@gmail.com>
2023-08-02 23:04:35 -0500

    Modularized eqObjects.js and refactored to use Chai and Mocha for testing

--- eqObjects.js
+++ eqObjects.js
@@ -1,38 +1,7 @@
-// ASSERT EQUAL FUNCTION FROM PREVIOUS EXERCISE
+const eqArrays = require('./eqArrays');
 
-const assertEqual = function(actual, expected) {
+// eqObjects takes in 2 objects and return true if they are equal and false otherwise.
 
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`👎 👎 👎 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-// EQARRAYS FUNCTION FROM PREVIOUS EXERCISE
-
-function eqArrays(array1, array2) {
-  let isEqual;
-  if (array1.length !== array2.length) {
-    isEqual = false;
-    return isEqual;
-  } else if (array1.length === array2.length) {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        //console.log(`Arrays are not equal: Array 1, position ${i} (${array1[i]}) does not equal Array 2, position ${i} (${array1[i]})`);
-        isEqual = false;
-        //console.log(isEqual);
-      } else {
-        //console.log("Arrays are equal")
-        isEqual = true;
-      }
-
-    }
-  } return isEqual;
-}
-
-
-// EQOBJECTS SOLUTION
 
 const eqObjects = function(object1, object2) {
   if ((Object.keys(object1).length) !== (Object.keys(object2).length)) { // Guard - Check to see if objects have different number of keys 
@@ -51,22 +20,4 @@ const eqObjects = function(object1, object2) {
   return true;
 }
 
-
-// TEST CASES
-
-const shirtObject = { color: "red", size: "medium" };
-const anotherShirtObject= { size: "medium", color: "red" };
-const longSleeveShirtObject= { size: "medium", color: "red", sleeveLength: "long" };
-const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
-const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] };
-const longSleeveMultiColorShirtObject= { size: "medium", colors: ["red", "blue"], sleeveLength: "long" };
-
-console.log(eqObjects(shirtObject, anotherShirtObject) + "should be TRUE"); // => true
-assertEqual(eqObjects(shirtObject, anotherShirtObject), true); // PASS same keys and values
-console.log(eqObjects(shirtObject, longSleeveShirtObject) + "should be FALSE"); // FALSE - Fails because the two objects have a different amount of keys
-assertEqual(eqObjects(shirtObject, longSleeveShirtObject), false); // PASS (correctly asserted that shirtObject and longSleeveShirtObject are not equal)
-assertEqual(eqObjects(multiColorShirtObject, anotherMultiColorShirtObject), true); // PASS - correctly compares the values of two objects, including handling for array values
-assertEqual(eqObjects(multiColorShirtObject, longSleeveMultiColorShirtObject), false); // PASS - correctly asserts that multiColorShirtObject and longSleeveMultiColorShirtObject are not equal
-
-console.log(eqObjects(multiColorShirtObject, anotherMultiColorShirtObject)); // => true
-console.log(eqObjects(multiColorShirtObject, longSleeveMultiColorShirtObject)); // => false
+module.exports = eqObjects;
---
+++ test/eqObjectsTest.js
@@ -0,0 +1,28 @@
+const eqObjects = require('../eqObjects');
+const assert = require('chai').assert;
+
+
+const shirtObject = { color: "red", size: "medium" };
+const anotherShirtObject= { size: "medium", color: "red" };
+const longSleeveShirtObject= { size: "medium", color: "red", sleeveLength: "long" };
+const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
+const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] };
+const longSleeveMultiColorShirtObject= { size: "medium", colors: ["red", "blue"], sleeveLength: "long" };
+
+
+
+describe("#eqObjects", () => {
+  it("returns TRUE if the two objects are exactly equal - { color: 'red', size: 'medium' } === { size: 'medium', color: 'red' }", () => {
+    assert.strictEqual(eqObjects(shirtObject, anotherShirtObject), true);
+  });
+
+  it("returns FALSE if the two objects have a different number of keys - { color: 'red', size: 'medium' } !== { size: 'medium', color: 'red', sleeveLength: 'long' }", () => {
+    assert.strictEqual(eqObjects(shirtObject, longSleeveShirtObject), false);
+  });
+
+  it("returns TRUE if the two objects contain array values and are exactly equal - { colors: ['red', 'blue'], size: 'medium' } === { size: 'medium', colors: ['red', 'blue'] }", () => {
+    assert.strictEqual(eqObjects(multiColorShirtObject, anotherMultiColorShirtObject), true);
+  });
+  
+ 
+});

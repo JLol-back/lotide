841f14dd670606c006fc2ed7dd5862955a30cf44
Jake Lollback <jalollback@gmail.com>
2023-08-04 16:53:52 -0500

    Added new test cases

--- test/tailTest.js
+++ test/tailTest.js
@@ -3,21 +3,21 @@ const assert = require('chai').assert;
 
 describe("#tail", () => {
   it("returns ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
-    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"] );
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
   });
 
   it("returns an empty array '[]' when provided an array with only one element", () => {
-    assert.deepEqual(tail(["Hello"]), [] );
+    assert.deepEqual(tail(["Hello"]), []);
   });
 
   it("returns an empty array '[]' when provided an empty array '[]'", () => {
-    assert.deepEqual(tail([]), [] );
+    assert.deepEqual(tail([]), []);
   });
 
-  it("returns a new array and does not modify the original array that is passed in", () => {
+  it("does not modify the original array that is passed in", () => {
     const words = ["Yo Yo", "Lighthouse", "Labs"];
     tail(words);
-    assert.deepEqual(words, ["Yo Yo", "Lighthouse", "Labs"] );
+    assert.deepEqual(words, ["Yo Yo", "Lighthouse", "Labs"]);
   });
 
 });  

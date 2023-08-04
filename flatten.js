74b161fd03cd481a16a5f2dd5b6299a4e98f73db
Jake Lollback <jalollback@gmail.com>
2023-08-04 16:53:01 -0500

    Refactored flatten to be a recursive solution to handle nested arrays

--- flatten.js
+++ flatten.js
@@ -1,20 +1,16 @@
 // flatten takes in an array that contains elements including nested array 
 // of elements and returns a flattened version of the array.
 
-flatten = function(array) {
-  let flatArray = [];                                // Define a new array to contain our new flattened array
-  for (let i = 0; i < array.length; i++) {           // Loop through outermost array
-    let arrCheck = Array.isArray(array[i]);          // Define array checker function using isArray method.
-    if (arrCheck) {                                  // If given element of outermost array is also an array
-      for (ii = 0; ii < array[i].length; ii++) {     // Start a second loop through the next level down array
-        flatArray.push(array[i][ii]);                // Push method to send each element we find to the flatArray variable
-      } 
+const flatten = function(array) {
+  let flatArray = [];
+  array.forEach((item) => {
+    if (Array.isArray(item)) {
+      flatArray = flatArray.concat(flatten(item));
+    } else {
+      flatArray.push(item);
     }
-    if (!arrCheck) {                                 // If the outermost element is not an array
-      flatArray.push(array[i]);                      // Push that value to the flatArray variable 
-    }
-  };
-  return flatArray;                                  // Once loop is finished, return our newly flattened array.
+  });
+  return flatArray;
 }
 
 module.exports = flatten;
--- test/flattenTest.js
+++ test/flattenTest.js
@@ -1,14 +1,17 @@
 const flatten = require('../flatten');
 const assert = require('chai').assert;
 
-
 describe("#flatten", () => {
   it("returns '[1, 2, 3, 4, 5, 6]' for '[1, 2, [3, 4], 5, [6]]'", () => {
     assert.deepEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
   });
 
-  it("returns '[1, 2, 3, 4, 5, 6]' for '[1, [2, [3], 4], 5 [6]]'", () => {
-    assert.deepEqual(flatten([1, [2, [3], 4], 5 [6]]), [1, 2, 3, 4, 5, 6]);
+  it("returns '[1, 2, 3, 4, 5, 6, 7, 8, 9]' for '[[1,2,3], [4,5,6], [7,8,9, [5,6]]]'", () => {
+    assert.deepEqual(flatten([[1,2,3], [4,5,6], [7,8,9, [5,6]]]), [1, 2, 3, 4, 5, 6, 7, 8, 9, 5, 6]);
+  });
+
+  it("returns '['A', 'B', 'C', 'D', 'E']' for '['A', [['B', ['C']], [[['D']], 'E']]]'", () => {
+    assert.deepEqual(flatten(['A', [['B', ['C']], [[['D']], 'E']]]), ['A', 'B', 'C', 'D', 'E']);
   });
 
 });

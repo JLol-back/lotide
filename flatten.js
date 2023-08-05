fc70f33dec2ea8507c75862dab377d627316cd7b
Jake Lollback <jalollback@gmail.com>
2023-08-05 17:23:09 -0500

    Add documentation comments and ran eslint for flatten.js

--- flatten.js
+++ flatten.js
@@ -1,3 +1,9 @@
+/**
+ * flatten takes in a array that contains elements including nested/multi-dimensional arrays of elements and returns a flattened version of the array.
+ * @param {*} array - Multi-dimensional array
+ * @returns {array} a one dimensional array containing all the values found in the nested array.
+ */
+
 const flatten = function(array) {
   let flatArray = [];
   array.forEach((item) => {
@@ -8,6 +14,6 @@ const flatten = function(array) {
     }
   });
   return flatArray;
-}
+};
 
 module.exports = flatten;

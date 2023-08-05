0aac6cf573f25194bf9420a76e1c2a5050f0e163
Jake Lollback <jalollback@gmail.com>
2023-08-05 16:57:18 -0500

    Add documentation comments and ran eslint for countLetters.js

--- countLetters.js
+++ countLetters.js
@@ -1,19 +1,22 @@
-//countLetters takes in a sentence (as a string) and then 
-// return a count of each of the letters in that sentence.
+/**
+ * countLetters takes in a string and returns a count of each of the letters present in the string.
+ * @param {*} string
+ * @returns an object containing one key for each unique letter found in the string with a value equal to how many times the letter was present within the string.
+ */
 
-const countLetters = function (string) {
+const countLetters = function(string) {
   let letterCount = {};
   let lowerCase = string.toLowerCase();
   for (const letter of lowerCase) {
-    if (letter !== ' ') { 
+    if (letter !== " ") {
       if (letterCount[letter]) {
         letterCount[letter] += 1;
       } else if (!letterCount[letter]) {
         letterCount[letter] = 1;
       }
+    }
   }
-}
   return letterCount;
-}
+};
 
 module.exports = countLetters;

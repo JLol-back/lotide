1c05f9875801e109291cd540e1841f3e91a76e8d
Jake Lollback <jalollback@gmail.com>
2023-08-05 17:27:35 -0500

    Add documentation comments and ran eslint for head.js

--- head.js
+++ head.js
@@ -1,9 +1,17 @@
-const head = function (array) { 
-  if (array.length === 0) {      // If array is empty, return undefined and function ends there.
+/**
+ * head takes in an array and returns the first element found within that array.
+ * @param {*} array
+ * @returns the first element itself, i.e. not as an array. An empty array will return undefined.
+ */
+
+const head = function(array) {
+  if (array.length === 0) {
+    // If array is empty, return undefined and function ends there.
     return undefined;
-  } else {                       // Otherwise return the 1st (0th) index of the array.
+  } else {
+    // Otherwise return the 1st (0th) index of the array.
     return array[0];
   }
-}
+};
 
 module.exports = head;

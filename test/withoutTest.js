5377cc4c8fe8ea42369561906503347e63208cb2
Jake Lollback <jalollback@gmail.com>
2023-08-05 09:45:11 -0500

    Add additional test cases to withoutTest.js

--- test/withoutTest.js
+++ test/withoutTest.js
@@ -17,6 +17,14 @@ describe("#without", () => {
     assert.deepEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]);
   });
 
+  it("returns ['1'] for array of ['1', '2', '2'] and items to remove of ['2']", () => {
+    assert.deepEqual(without([1, 2, 2], [2]), [1]);
+  });
+
+  it("returns ['3'] for array of ['1', '1', '3'] and items to remove of ['1']", () => {
+    assert.deepEqual(without([1, 1, 3], [1]), [3]);
+  });
+
   it("returns a NEW array and does not modify the original source array", () => {
     const words = ["hello", "world", "lighthouse"];
     without(words, ['lighthouse']);

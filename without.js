03e3a83480bcd2aa5ab20323f172d38bab7b1cab
Jake Lollback <jalollback@gmail.com>
2023-08-05 18:21:27 -0500

    Add documentation comments and ran eslint on without.js

--- without.js
+++ without.js
@@ -1,17 +1,16 @@
 /**
- *
+ * without takes in a source array and an itemsToRemove array and returns a subset of the source array, removing unwanted elements identified by itemsToRemove.
  * @param {array} source
  * @param {array} itemsToRemove
- * @returns
+ * @returns {array} with only those elements from source that are not present in the itemsToRemove array.
  */
 const without = function(source, itemsToRemove) {
-
-  const result = Array.from(source, x => x);
+  const result = Array.from(source, (x) => x);
 
   for (let i = 0; i < result.length; i++) {
     if (itemsToRemove.includes(result[i])) {
       result.splice(i, 1);
-      i--;          // If item removed from results array, decrement 'i' to account for the newly shortened results array length so our for loop doesn't end before iterating over every value.
+      i--; // If item removed from results array, decrement 'i' to account for the newly shortened results array length so our for loop doesn't end before iterating over every value.
     }
   }
   return result;

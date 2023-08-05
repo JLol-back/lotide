39e42920138966505818f04ad3db2497f4ba2d62
Jake Lollback <jalollback@gmail.com>
2023-08-05 18:14:45 -0500

    Add documentation comments and ran eslint on takeUntil.js

--- takeUntil.js
+++ takeUntil.js
@@ -1,14 +1,20 @@
-const takeUntil = function(array, callback) {
+/**
+ * takeUntil takes in an array and callback and returns a slice of the array based on the criteria specified in the callback.
+ * @param {array} array
+ * @param {callback function} callback
+ * @returns {array} containing elements that returned a truthy value after having the callback function applied to them.
+ */
 
+const takeUntil = function(array, callback) {
   const results = [];
-for (let item of array) {
-  if (callback(item) === false) {
-    results.push(item);
-  } else {
-    return results;
+  for (let item of array) {
+    if (callback(item) === false) {
+      results.push(item);
+    } else {
+      return results;
+    }
   }
-}
-return results;
+  return results;
 };
 
 module.exports = takeUntil;

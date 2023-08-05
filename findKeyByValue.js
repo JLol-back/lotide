d7b117cfa3a805353d19cd5834f4e739d0795c45
Jake Lollback <jalollback@gmail.com>
2023-08-05 17:19:39 -0500

    Add documentation comments and ran eslint for findKeyByValue.js

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,3 +1,10 @@
+/**
+ * findKeyByValue takes in an object and a value and scans the object to identify the first key which contains the given value.
+ * @param {*} object
+ * @param {*} value
+ * @returns {string} the first key that corresponds to the value passed into the function. If no key with that given value is found, then it should return undefined.
+ */
+
 const findKeyByValue = function(object, value) {
   for (const key in object) {
     if (object[key] === value) {

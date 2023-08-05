46db71e9b115d74c1cb1bb327aa438934cd16fca
Jake Lollback <jalollback@gmail.com>
2023-08-05 10:02:12 -0500

    Add handling for arrays with multiple values that meet removal criteria

--- without.js
+++ without.js
@@ -1,3 +1,9 @@
+/**
+ * 
+ * @param {array} source 
+ * @param {array} itemsToRemove 
+ * @returns 
+ */
 const without = function(source, itemsToRemove) {
 
   const result = Array.from(source, x => x);
@@ -5,14 +11,10 @@ const without = function(source, itemsToRemove) {
   for (let i = 0; i < result.length; i++) {
     if (itemsToRemove.includes(result[i])) {
       result.splice(i, 1);
+      i--;          // If item removed from results array, decrement 'i' to account for the newly shortened results array length so our for loop doesn't end before iterating over every value.
     }   
   }
   return result;
 };
 
-without([1, 2, 3], [4, 5, 1]) 
-
 module.exports = without;
-
-
-

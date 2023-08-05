59bc816108dd5ccd7f92a1aacdc7b962e50e984f
Jake Lollback <jalollback@gmail.com>
2023-08-05 09:22:57 -0500

    Remove unreachable code on lines 5,6

--- findKey.js
+++ findKey.js
@@ -2,9 +2,6 @@ const findKey = function(object, callback) {
   for (const key in object) {
     if (callback(object[key]) === true) {
       return key;
-      if (callback(object[key]) !== false) {
-        return undefined;
-      }
     }
   }
 };

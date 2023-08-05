d95c3bb648f90b4089dc3a6a1aec6218af408a5d
Jake Lollback <jalollback@gmail.com>
2023-08-05 10:44:28 -0500

    Remove redundant if statements from eqArrays.js

--- eqArrays.js
+++ eqArrays.js
@@ -1,17 +1,18 @@
 /**
- *
- * @param {*} array1
- * @param {*} array2
- * @returns
+ * eqArraystakes compares the values of 2 arrays to determine if they are equal.
+ * @param {array} array1
+ * @param {array} array2
+ * @returns boolean - true if the arrays are equal and false otherwise.
  */
 const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
+    // If arrays have different lengths, return false
     return false;
-  } else if (array1.length === array2.length) {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        return false;
-      }
+  }
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
     }
   }
   return true;

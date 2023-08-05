b1bbff76e8a45c08c68023fe8dcfb1ba2019ed3e
Jake Lollback <jalollback@gmail.com>
2023-08-05 18:10:19 -0500

    Add documentation and ran eslint on middle.js and tail.js

--- middle.js
+++ middle.js
@@ -1,25 +1,34 @@
+/**
+ * middle takes in an array and returns the middle most element of the array.
+ * @param {array} array
+ * @returns {array} with only the middle element(s) of the provided array. For arrays with one or two elements, an empty array is returned.
+ * Returns an array with two elements when provided with an array with an even number of elements.
+ */
 
 const middle = function(array) {
-  result = [];  
+  let result = [];
   if (!Array.isArray(array)) {
-    return "Error: Not an array"
+    return "Error: Not an array";
   }
 
-  if (array.length <= 2) {                      //LOGIC FOR ONE OR TWO ELEMENTS
+  if (array.length <= 2) {
+    //LOGIC FOR ONE OR TWO ELEMENTS
     return [];
   }
 
-  if (array.length % 2 !== 0) {                //LOGIC FOR ODD NUMBER OF ELEMENTS
-    let x = Math.floor(array.length / 2);      // Half the array length and round down to the nearest whole number
+  if (array.length % 2 !== 0) {
+    //LOGIC FOR ODD NUMBER OF ELEMENTS
+    let x = Math.floor(array.length / 2); // Half the array length and round down to the nearest whole number
     result.push(array[x]);
   }
 
-  if (array.length % 2 == 0) {                 // LOGIC FOR EVEN NUMBER OF ELEMENTS
+  if (array.length % 2 === 0) {
+    // LOGIC FOR EVEN NUMBER OF ELEMENTS
     let x = array.length / 2;
-    result.push(array[x-1]);
+    result.push(array[x - 1]);
     result.push(array[x]);
   }
   return result;
-}
+};
 
 module.exports = middle;
--- tail.js
+++ tail.js
@@ -1,3 +1,9 @@
+/**
+ * tail takes in an array and returns every element except the (first element) of the array.
+ * @param {array} array
+ * @returns {array} every element except the head (first element) of the array.
+ */
+
 const tail = function(array) {
   let newArray = array.slice(1);
   return newArray;

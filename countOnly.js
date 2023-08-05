e0db37be3a78f836df0656584eb29fe059bf2383
Jake Lollback <jalollback@gmail.com>
2023-08-05 17:03:44 -0500

    Add documentation comments and ran eslint for countOnly.js

--- countOnly.js
+++ countOnly.js
@@ -1,5 +1,10 @@
-
-const countOnly = function (allItems, itemsToCount) {
+/**
+ * countOnly takes in an array and an object and returns counts of a subset of the array's items as defined by the input object.
+ * @param {array} allItems
+ * @param {object} itemsToCount
+ * @returns a NEW object containing counts for how many times each key listed in the itemsToCount object appeared within the allItems array.
+ */
+const countOnly = function(allItems, itemsToCount) {
   const results = {};
 
   for (const item of allItems) {
@@ -11,7 +16,7 @@ const countOnly = function (allItems, itemsToCount) {
       }
     }
   }
-    return results;
-  }
+  return results;
+};
 
 module.exports = countOnly;
